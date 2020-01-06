1fa464285a2e64f6bd99fc627fa7cbcf32c4c429
Dnniz <dannuzp@gmail.com>
2020-01-05 23:17:20 -0500

    Implementando vista parcial

---
+++ AspNetCore/Controllers/AsignaturaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Models;
+using CSharpNetCore.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new JsonDTO();
+            model.Asignaturas = new List<Asignatura>();
+
+
+            return View();
+        }
+    }
+}
---
+++ AspNetCore/Models/JsonDTO.cs
@@ -0,0 +1,16 @@
+﻿using CSharpNetCore.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Models
+{
+    public class JsonDTO
+    {
+        public Escuela Escuela { get; set; }
+        public Asignatura Asignatura { get; set; }
+        public List<Asignatura> Asignaturas { get; set; }
+        public List<Escuela> Escuelas { get; set; }
+    }
+}
